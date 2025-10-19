08bed836f58d29786569a4f41b3bbce9346e6d6f
Tylor8591 <gaogaixue@163.com>
2025-10-19 18:15:09 +0800

    use dynamic DEVICE_DTS

--- target/linux/zynq/image/yourboard.mk
+++ target/linux/zynq/image/yourboard.mk
@@ -1,9 +1,8 @@
-define Device/your_vendor_zynq-your_board
+define Device/your_vendor_zynq-your-board
 	$(call Device/FitImageGzip)
 	DEVICE_VENDOR := YourVendor
 	DEVICE_MODEL := YourBoard
-	DEVICE_DTS := zynq-your-board
 	IMAGES := sdcard.img.gz
 	IMAGE/sdcard.img.gz := zynq-sdcard | gzip
 endef
-TARGET_DEVICES += your_vendor_zynq-your_board
+TARGET_DEVICES += your_vendor_zynq-your-board
